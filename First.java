13873ce9f38fa488ba4eab7d975f8297c5585d0b
shaku nagaraju <nagfriends.s@gmail.com>
2021-04-27 21:18:59 +0530

    java progrma adding

---
+++ First.java
@@ -0,0 +1,13 @@
+class First{
+
+public static voi	d main(String x[])
+{
+
+System.out.println("welcome first java program");
+
+}
+
+
+
+
+}
---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+class Helloworld{
+
+public static void main(Strings args[]){
+
+System.out.println("welcome the first java programs")
+
+}
+
+
+
+
+}
